cfbc1bab918aec828a67347fb8a84e1e9927c850
GomeBM <gomebenmoshe@gmail.com>
2024-12-31 11:42:32 +0200

    Changed some manifest stuff

--- frontend/src/components/CustomSelect/CustomSelect.js
+++ frontend/src/components/CustomSelect/CustomSelect.js
@@ -1,30 +1,3 @@
-// import React from "react";
-// import "./CustomSelect.css";
-
-// const CustomSelect = ({
-//   value,
-//   onChange,
-//   options = [], // Provide default empty array
-//   placeholder,
-//   className = "select",
-// }) => {
-//   // Ensure options is an array and filter out any null/undefined values
-//   const safeOptions = Array.isArray(options) ? options.filter(Boolean) : [];
-
-//   return (
-//     <select value={value} onChange={onChange} className={className}>
-//       <option value="">{placeholder}</option>
-//       {safeOptions.map((option) => (
-//         <option key={option} value={option}>
-//           {option}
-//         </option>
-//       ))}
-//     </select>
-//   );
-// };
-
-// export default CustomSelect;
-
 import React from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import "./CustomSelect.css";
